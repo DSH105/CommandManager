f87f7592cc2a2cdbc4f3455908986835381ec3a0
DSH105 <jason.chirgwin@icloud.com>
2014-06-29 15:19:26 +1000

    Add an example using variables

--- src/main/java/com/dsh105/command/example/ExamplePlugin.java
+++ src/main/java/com/dsh105/command/example/ExamplePlugin.java
@@ -21,6 +21,8 @@ import com.dsh105.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+
 public class ExamplePlugin extends JavaPlugin {
 
     protected ICommandManager PRETEND_MANAGER;
@@ -77,6 +79,17 @@ public class ExamplePlugin extends JavaPlugin {
         )
         public boolean playerOnlyCommand(CommandEvent<Player> event) {
             event.respond("Looks like you're logged in correctly :)");
+            return true;
+        }
+
+        @Command(
+                command = "variable <var>",
+                description = "Test variables",
+                permission = "example.command.variable",
+                aliases = "v <var>"
+        )
+        public boolean variableCommand(CommandEvent event) {
+            event.respond("You entered a variable: " + event.variable("var"));
             return true;
         }
 
